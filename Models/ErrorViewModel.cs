0a37a433b26b83e06b6815bd92013e28baaf3365
Purodhika Sharma <pshar245@my.centennialcollege.ca>
2023-11-20 14:25:54 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Purodhika_Devops_Quiz4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
